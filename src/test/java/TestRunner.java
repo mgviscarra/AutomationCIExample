e2db30443bb9e3958b127484c4256397995e45ad
Mauricio Viscarra <mgviscarra@gmail.com>
2018-06-01 23:22:11 -0400

    Adding few changes to test Runner.java

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,10 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber","json:target/cucumber.json"},
+        features = {"src/test/java/features"})
+public class TestRunner {
+
+}
